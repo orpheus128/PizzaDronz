3cb4d4632b0fcba74a3ba14aa6b7d111bf5785c5
orpheus128 <danlindsay21@gmail.com>
2023-10-12 15:48:34 +0100

    tests

--- src/main/java/uk/ac/ed/inf/LngLatHandler.java
+++ src/main/java/uk/ac/ed/inf/LngLatHandler.java
@@ -1,13 +1,21 @@
 package uk.ac.ed.inf;
 
+import uk.ac.ed.inf.ilp.constant.CentralRegionVertexOrder;
 import uk.ac.ed.inf.ilp.data.LngLat;
 import uk.ac.ed.inf.ilp.data.NamedRegion;
+
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
 
 public class LngLatHandler implements uk.ac.ed.inf.ilp.interfaces.LngLatHandling {
 
     @Override
     public double distanceTo(LngLat startPosition, LngLat endPosition) {
-
+        /**
+         * Calculate distance from one point to another using the pythagorean theorem
+         * @param startPosition is the position you are calculating the distance from
+         * @param endPosition is the position you are calculating the distance to
+         */
 
         //Here, I store the longitude and latitude from the start and end positions in variables as it prevents the
         //final calculation being messy
@@ -24,6 +32,11 @@ public class LngLatHandler implements uk.ac.ed.inf.ilp.interfaces.LngLatHandling
 
     @Override
     public boolean isCloseTo(LngLat startPosition, LngLat otherPosition) {
+        /**
+         * as per the specification, if a point is within 0.00015 of another point, it is considered close
+         * @param startPosition is the first position
+         * @param otherPosition is the position you are checking is close to the first position
+         */
 
         return distanceTo(startPosition, otherPosition) < 0.00015;
     }
@@ -41,19 +54,47 @@ public class LngLatHandler implements uk.ac.ed.inf.ilp.interfaces.LngLatHandling
 
     @Override
     public boolean isInRegion(LngLat position, NamedRegion region) {
+        /**
+         * Uses the ray casting algorithm to test if there are any intersections,
+         * if the number of intersections is even, the point lies outwith the region
+         * if the number of intersections is odd, the point lies within the region
+         * @param position is the current position you are checking
+         * @param region is the region that the position may or may not be in
+         */
 
-        region.name();
-        region.vertices();
+        LngLat[] vertices = region.vertices();
+        double x1;
+        double x2;
+        double y1;
+        double y2;
+        int numIntersections = 0;
 
+        for (int i = 0; i < vertices.length; i++){
+            x1 = vertices[i].lng();
+            x2 = vertices[((i+1) % vertices.length)].lng();
+            y1 = vertices[i].lat();
+            y2 = vertices[((i+1) % vertices.length)].lat();
 
-        //Need to have the position lat and long be within bounds
-        return false;
+            if ((y1 > position.lat()) != (y2 > position.lat()) &&
+                    ((position.lng() < (x2 - x1) * (position.lat() - y1) / (y2 - y1) + x1))){
+                numIntersections++;
+            }
+        }
+
+        return numIntersections % 2 == 1;
     }
 
     @Override
     public LngLat nextPosition(LngLat startPosition, double angle) {
+        /**
+         * returns the next position the drone will move to when given an angle which represents one of the 16 major
+         * compass directions
+         * @param startPosition is the initial position
+         * @param angle is the direction the drone will move in (East is 0, North is 90, etc)
+         */
 
-        if (angle == 999){return startPosition; }
+        if (angle == 999){return startPosition;}
+        else if(angle % 22.5 != 0){return startPosition;}
         else{
             double lng = (0.00015 * Math.cos(angle) + startPosition.lng());
             double lat = (0.00015 * Math.sin(angle) + startPosition.lat());
--- src/main/java/uk/ac/ed/inf/OrderValidator.java
+++ src/main/java/uk/ac/ed/inf/OrderValidator.java
@@ -4,6 +4,7 @@ import uk.ac.ed.inf.ilp.constant.OrderStatus;
 import uk.ac.ed.inf.ilp.constant.OrderValidationCode;
 import uk.ac.ed.inf.ilp.data.CreditCardInformation;
 import uk.ac.ed.inf.ilp.data.Order;
+import uk.ac.ed.inf.ilp.data.Pizza;
 import uk.ac.ed.inf.ilp.data.Restaurant;
 
 import java.util.Arrays;
@@ -13,80 +14,40 @@ import static java.lang.Integer.parseInt;
 public class OrderValidator implements uk.ac.ed.inf.ilp.interfaces.OrderValidation {
     @Override
     public Order validateOrder(Order orderToValidate, Restaurant[] definedRestaurants) {
-
-        //credit card number (16 digits, use regex to strip down to just numbers) - done
-        //cvv (3 digits) - done
-        //expiration date
-        //the menu items selected in the order
-        //the involved restaurants
-        //if the maximum count is exceeded
-        //if the order is valid on the given date for the involved restaurants (opening days)
+        /**
+         * validates an order by checking specific aspects of it
+         * if any of these aspects are erroneous an associated error code will be returned which indicates the error
+         * @param orderToValidate is the order which we are inspecting
+         * @param definedRestaurants is a list of restaurants which are participating in the PizzaDronz service
+         */
 
         CreditCardInformation creditCard = orderToValidate.getCreditCardInformation();
 
-        String ccn = creditCard.getCreditCardNumber();
-        ccn = ccn.replaceAll("\\s", "");
-
-        if (ccn.length() != 16){
+       
+        if (checkCCNInvalid(creditCard)){
+            orderToValidate.setOrderStatus(OrderStatus.INVALID);
             orderToValidate.setOrderValidationCode(OrderValidationCode.CARD_NUMBER_INVALID);
-            orderToValidate.setOrderStatus(OrderStatus.INVALID);
             return orderToValidate;
         }
-        for (char c : ccn.toCharArray()){
-            if (!Character.isDigit(c)){
-                orderToValidate.setOrderValidationCode(OrderValidationCode.CARD_NUMBER_INVALID);
-                orderToValidate.setOrderStatus(OrderStatus.INVALID);
-                return orderToValidate;
-            }
-        }
 
-        String cvv = creditCard.getCvv();
-        cvv = cvv.replaceAll("//s", "");
-
-        if (cvv.length() != 3){
+        if (checkCVVInvalid(creditCard)){
+            orderToValidate.setOrderStatus(OrderStatus.INVALID);
             orderToValidate.setOrderValidationCode(OrderValidationCode.CVV_INVALID);
-            orderToValidate.setOrderStatus(OrderStatus.INVALID);
             return orderToValidate;
         }
-        for (char c : cvv.toCharArray()) {
-            if (!Character.isDigit(c)) {
-                orderToValidate.setOrderValidationCode(OrderValidationCode.CVV_INVALID);
-                orderToValidate.setOrderStatus(OrderStatus.INVALID);
-                return orderToValidate;
-            }
-        }
 
-        String expiry = creditCard.getCreditCardExpiry();
-        if (!expiry.matches("(0[1-9]|1[0-2])/[0-9][0-9]")){
+        if (checkExpiryDateInvalid(orderToValidate, creditCard)){
+            orderToValidate.setOrderStatus(OrderStatus.INVALID);
             orderToValidate.setOrderValidationCode(OrderValidationCode.EXPIRY_DATE_INVALID);
-            orderToValidate.setOrderStatus(OrderStatus.INVALID);
-            return orderToValidate;
-        }
-        String[] splitExpiry = expiry.split("/");
-
-        String orderYear = String.valueOf(orderToValidate.getOrderDate().getYear());
-        int orderYearInt = parseInt(orderYear.substring(2));
-        int orderMonth = java.time.LocalDate.now().getMonth().getValue();
-
-        if (parseInt(splitExpiry[1]) < orderYearInt){
-            orderToValidate.setOrderValidationCode(OrderValidationCode.EXPIRY_DATE_INVALID);
-            orderToValidate.setOrderStatus(OrderStatus.INVALID);
-            return orderToValidate;
-        }else if(parseInt(splitExpiry[0]) < orderMonth & parseInt(splitExpiry[1]) == orderYearInt){
-            orderToValidate.setOrderValidationCode(OrderValidationCode.EXPIRY_DATE_INVALID);
-            orderToValidate.setOrderStatus(OrderStatus.INVALID);
             return orderToValidate;
         }
 
-        //checkPizza will check for pizza related errors, such as the maximum count being exceeded, or the pizza being
-        //undefined, or the restaurant being closed
-        OrderValidationCode code = checkPizza(orderToValidate, definedRestaurants);
-        if (code != OrderValidationCode.UNDEFINED){
-            orderToValidate.setOrderValidationCode(code);
+        OrderValidationCode pizzaCode = checkPizza(orderToValidate, definedRestaurants);
+        if (pizzaCode != OrderValidationCode.UNDEFINED){
+            orderToValidate.setOrderValidationCode(pizzaCode);
             orderToValidate.setOrderStatus(OrderStatus.INVALID);
             return orderToValidate;
         }
-
 
         orderToValidate.setOrderValidationCode(OrderValidationCode.NO_ERROR);
         orderToValidate.setOrderStatus(OrderStatus.VALID_BUT_NOT_DELIVERED);
@@ -94,15 +55,103 @@ public class OrderValidator implements uk.ac.ed.inf.ilp.interfaces.OrderValidati
         return orderToValidate;
     }
 
+    public Boolean checkCCNInvalid(CreditCardInformation creditCard){
+        /**
+         * returns true if there are issues with the credit card number, and false if there are not
+         * @param creditCard is the credit card from the order which we extract the credit card number from
+         */
+        String ccn = creditCard.getCreditCardNumber();
+        if (ccn == null){
+            return true;
+        }
+        ccn = ccn.replaceAll("\\s", "");
+
+        if (ccn.length() != 16){
+            return true;
+        }
+        for (char c : ccn.toCharArray()){
+            if (!Character.isDigit(c)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean checkCVVInvalid(CreditCardInformation creditCard){
+        /**
+         * returns true if there are issues with the credit card's cvv, and false if there are not
+         * @param creditCard is the credit card which we extract the cvv from
+         */
+        String cvv = creditCard.getCvv();
+
+        if (cvv == null){
+            return true;
+        }
+        cvv = cvv.replaceAll("//s", "");
+
+        if (cvv.length() != 3){
+            return true;
+        }
+        for (char c : cvv.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean checkExpiryDateInvalid(Order orderToValidate, CreditCardInformation creditCard){
+        /**
+         * returns true if the expiry date is invalid for any reason, such as being in the past or
+         * being in the wrong format. returns false if there are no issues with the cvv
+         * @param orderToValidate is the order with a date which we are checking the expiry date against
+         * @param creditCard is the credit card we are extracting the expiry date from
+         */
+
+        String expiry = creditCard.getCreditCardExpiry();
+        if (expiry == null){
+            return true;
+        }
+        if (!expiry.matches("(0[1-9]|1[0-2])/[0-9][0-9]")){
+            return true;
+        }
+
+        String[] splitExpiry = expiry.split("/");
+
+        String orderYear = String.valueOf(orderToValidate.getOrderDate().getYear());
+        int orderYearInt = parseInt(orderYear.substring(2));
+        int orderMonth = java.time.LocalDate.now().getMonth().getValue();
+
+        if (parseInt(splitExpiry[1]) < orderYearInt){
+            return true;
+        }else if(parseInt(splitExpiry[0]) < orderMonth & parseInt(splitExpiry[1]) == orderYearInt){
+            return true;
+        }
+
+        return false;
+    }
+
+    
+    //The following code 
     public OrderValidationCode checkPizza(Order orderToValidate, Restaurant[] definedRestaurants){
+        /**
+         * loops through the menus of every restaurant until finding the first pizza from an order
+         * once this pizza is found, the restaurant is saved, and then the other pizzas are searched for
+         * if no pizzas are found, then an error is thrown
+         * if the pizzas come from different restaurants, a separate error is thrown.
+         * if neither of these are true, a further function is used to check for more errors
+         * if no errors are found, the default code is returned to the main function
+         * @param orderToValidate is the order the pizzas are taken from
+         * @param definedRestaurant is the list of restaurants which menus are taken from
+         */
 
         uk.ac.ed.inf.ilp.data.Pizza[] pizzasOrdered = orderToValidate.getPizzasInOrder();
-        Restaurant restaurant = definedRestaurants[0];
-        boolean pizzaFound = false;
-        int menuCount = 0;
-        int restCount = 0;
+        Restaurant restaurant;
+        boolean pizzaFound;
+        int menuCount;
+        int restCount;
         int pizzaCount = 0;
-        Restaurant tempRestaurant = null;
+        Restaurant tempRestaurant;
         int total = 100;
         uk.ac.ed.inf.ilp.data.Pizza[] menu;
 
@@ -114,7 +163,7 @@ public class OrderValidator implements uk.ac.ed.inf.ilp.interfaces.OrderValidati
                 menuCount = 0;
                 while (!pizzaFound & menuCount < menu.length) {
 
-                    if (pizzasOrdered[pizzaCount].equals(menu[menuCount])) {
+                    if (pizzasOrdered[pizzaCount].equals(menu[menuCount])){
                         pizzaFound = true;
                         tempRestaurant = definedRestaurants[restCount];
                         total += pizzasOrdered[pizzaCount].priceInPence();
@@ -129,12 +178,34 @@ public class OrderValidator implements uk.ac.ed.inf.ilp.interfaces.OrderValidati
             if (!pizzaFound){
                 return OrderValidationCode.PIZZA_NOT_DEFINED;
             }
-            if (restaurant != tempRestaurant){
+            if (!restaurant.equals(tempRestaurant)){
                 return OrderValidationCode.PIZZA_FROM_MULTIPLE_RESTAURANTS;
             }
             pizzaCount++;
         }
 
+        OrderValidationCode extraPizzaCode = checkPizzaFurther(restaurant, orderToValidate,pizzasOrdered, total);
+
+        if (extraPizzaCode != OrderValidationCode.UNDEFINED){
+            return extraPizzaCode;
+        }
+
+        //the function will return UNDEFINED to show that no errors have been found within the pizza information
+        return OrderValidationCode.UNDEFINED;
+    }
+
+    OrderValidationCode checkPizzaFurther(Restaurant restaurant, Order orderToValidate, Pizza[] pizzasOrdered, int total){
+        /**
+         * checks for more errors which could occur
+         * checks if the restaurant was open when the order was placed
+         * checks if there are no more than 4 pizzas ordered
+         * checks if the total amount is correct
+         * if all of these checks pass, then the default code is returned
+         * @param restaurant is the restaurant that is ordered from
+         * @param orderToValidate is the order we extract the orderDate from
+         * @param pizzasOrdered is a list of the pizzas ordered
+         * @param total is a computed running total of the pizza's prices from the menu
+         */
         if (!Arrays.asList(restaurant.openingDays()).contains(orderToValidate.getOrderDate().getDayOfWeek())){
             return OrderValidationCode.RESTAURANT_CLOSED;
         }
@@ -144,7 +215,7 @@ public class OrderValidator implements uk.ac.ed.inf.ilp.interfaces.OrderValidati
         if (total != orderToValidate.getPriceTotalInPence()){
             return OrderValidationCode.TOTAL_INCORRECT;
         }
-        //the function will return UNDEFINED to show that no errors have been found within the pizza information
         return OrderValidationCode.UNDEFINED;
     }
+
 }
